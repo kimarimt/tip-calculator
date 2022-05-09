8833b021c25070300dc8c3c2babdf39733bc3176
Alec German <kimarimt@protonmail.com>
2022-05-09 15:25:27 -0400

    implemented the tip calculator

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 def main():
-    print('Hello, World')
+    print('Welcome to the Tip Calculator')
+    pretotal = float(input('Total Bill: $'))
+    party_size = int(input('Number of people to split bill: '))
+    tip = float(input('Tip (10, 12, or 15): ')) / 100
+    total_per_person = ((pretotal * tip) + pretotal) / party_size
+    print(f'Each person should pay: ${total_per_person:.2f}')
 
 
 if __name__ == '__main__':
